1d8c6d56e2b6e16f5bbe6e80c0d1e2b9c8761115
akshata <akshata2018p@gamil.com>
2023-02-08 22:04:07 +0530

    [akshata]Add:FibonaaciSeries

---
+++ LogocalProgram/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgram
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1 : FibonacciSeries\n ");
+            Console.WriteLine("Enter a Option");
+            int Option = Convert.ToInt32(Console.ReadLine());
+            switch (Option)
+            {
+                case 1:
+                    FibonacciSeries Num = new FibonacciSeries();
+                    Num.Series();
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
